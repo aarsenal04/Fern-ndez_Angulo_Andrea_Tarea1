32d1e1d41f14f025230ccaa8fb5549f83491a3d0
Andrea Fernández <andreafernez@estudiantec.cr>
2021-08-29 22:46:08 -0600

    correcciones finales
    casi terminado

--- src/Gui/AppDescuento.java
+++ src/Gui/AppDescuento.java
@@ -8,6 +8,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+/**
+
+ * La interfaz se realizó con Java Swing.
+ * Esta clase se define como {@link AppDescuento} con sus respectivos sockets y funcionalidades.
+ * Como se menciona en el Main, esta ventana surge de la acción en los botones de cliente y servidor.
+ * Contiene las entradas de texto para valor, peso y tax (impuesto).
+ * Contiene la entrada del chat por donde se intercambia la información requerida.
+
+ */
+
 public class AppDescuento extends JFrame{
 
     private JPanel VentanaC;
@@ -24,23 +34,23 @@ public class AppDescuento extends JFrame{
         int selecter = i;
 
         setContentPane(VentanaC);
-        setSize(500,500);
+        setSize(500,500); // tamaño de la ventana
         setTitle(title);
         setVisible(true);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // para poder cerrar la ventana
 
         if(i == 0){
             c = new Cliente(textPane1);
-            Thread threadCliente = new Thread(c);
+            Thread threadCliente = new Thread(c); // hilo del cliente
             threadCliente.start();
         }else{
             s = new Servidor(textPane1);
-            Thread threadCliente = new Thread(s);
+            Thread threadCliente = new Thread(s); // hilo del servidor
             threadCliente.start();
 
         }
 
-        enviarButton.addActionListener(new ActionListener() {
+        enviarButton.addActionListener(new ActionListener() { // capta acciones
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -48,12 +58,12 @@ public class AppDescuento extends JFrame{
                 msg = "C#" + textField1.getText() + "#" + textField2.getText() + "#" + textField3.getText();
 
                 if(selecter == 0){
-                    textPane1.setText(textPane1.getText() + "\n" + "[Client] The value are: " + textField1.getText() + " " + textField2.getText() + " " + textField3.getText());
+                    textPane1.setText(textPane1.getText() + "\n" + "[Client] The value are: " + textField1.getText() + " " + textField2.getText() + " " + textField3.getText()); // valor enviado por el cliente
                     c.Send(msg);
                 }else{
                     try {
                         s.Send(msg);
-                        textPane1.setText(textPane1.getText() + "\n" + "[Server] The value are: " + textField1.getText() + " " + textField2.getText() + " " + textField3.getText());
+                        textPane1.setText(textPane1.getText() + "\n" + "[Server] The value are: " + textField1.getText() + " " + textField2.getText() + " " + textField3.getText()); // valor enviado por el servidor
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
                     }
--- src/Gui/selecter.java
+++ src/Gui/selecter.java
@@ -3,9 +3,16 @@ package Gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
-import Gui.AppDescuento;
+
+/**
+
+ * La interfaz se realizó con Java Swing.
+ * Esta clase se define como {@link selecter} con sus respectivos sockets y funcionalidades.
+ * Como se menciona en el Main, el selecter es la primer ventana que aparece.
+ * Contiene los botones de Cliente y Servidor
+
+ */
 
 public class selecter extends JFrame{
 
@@ -18,25 +25,25 @@ public class selecter extends JFrame{
         JFrame app = new JFrame("Selección");
 
         app.setContentPane(Canva);
-        app.setSize(500,500);
+        app.setSize(500,500); // tamaño de la ventana axh
         app.setTitle("Ventana Servidor y Cliente");
         app.setVisible(true);
-        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // para poder cerrar la ventana
 
-        clienteButton.addActionListener(new ActionListener() {
+        clienteButton.addActionListener(new ActionListener() { // capta la acción
             @Override
             public void actionPerformed(ActionEvent e) {
                 app.setVisible(false);
                 app.dispose();
-                AppDescuento app = new AppDescuento(0,"Cliente");
+                AppDescuento app = new AppDescuento(0,"Cliente"); // botón de cliente
             }
         });
-        servidorButton.addActionListener(new ActionListener() {
+        servidorButton.addActionListener(new ActionListener() { // capta la acción
             @Override
             public void actionPerformed(ActionEvent e) {
                 app.setVisible(false);
                 app.dispose();
-                AppDescuento app = new AppDescuento(1,"Servidor");;
+                AppDescuento app = new AppDescuento(1,"Servidor"); // botón de servidor
             }
         });
     }
